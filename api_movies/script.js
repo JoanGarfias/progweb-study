cdf014a4ac86f07a8f3f312c2aa5dbd32ff751da
Joan Pablo <113860964+joansk09@users.noreply.github.com>
2025-04-04 00:45:41 -0600

    feature: API de peliculas con template para detalles

--- api_movies/script.js
+++ api_movies/script.js
@@ -1,5 +1,9 @@
 let catalogo = document.querySelector(".catalogo");
-const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+let buscarBtn = document.querySelector(".btnBuscar");
+let movieInput = document.querySelector(".movieInput");
+console.log(movieInput);
+
+const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 const options = {
   method: 'GET',
   headers: {
@@ -33,7 +37,7 @@ function crearPelicula(id, titulo, fecha, descripcion, imgurl){
 
     let btn = document.createElement("a");
     btn.className = "btn btn-primary";
-    btn.href = "#";
+    btn.href = `detalles.html?id=${id}`;
     btn.textContent = "Ver más";
 
     cardBody.appendChild(cardTitle);
@@ -48,9 +52,31 @@ function crearPelicula(id, titulo, fecha, descripcion, imgurl){
 fetch(url, options)
   .then(res => res.json())
   .then(json => {
+    console.log(json);
     json.results.forEach(e => {
         const urlImg = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" + e.poster_path;
-        crearPelicula(e.id, e.title, e.release_date, e.overview, urlImg)
+        crearPelicula(e.id, e.title, e.release_date, e.overview, urlImg);
+        //console.log(e);
     });
   })
   .catch(err => console.error(err));
+
+
+buscarBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let idBuscar = movieInput.value;
+    console.log("El ID a buscar es: " + idBuscar);
+    const url = `https://api.themoviedb.org/3/movie/${idBuscar}?language=en-US`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MTI2NmNiOTk2MWQ2ZWM2OWQ0ZTgwZjVhNzRmY2NjMiIsIm5iZiI6MTc0MzMxNTAxNi41MzEwMDAxLCJzdWIiOiI2N2U4ZTA0ODRmNzQxYzc1YmJjNjkxNTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ck84u2Bxt133g5zjo7SPuAFxi9ppnQYmyoKRaWWOILs'
+      }
+    };
+    fetch(url, options)
+    .then(data => data.json())
+    .then(res => {
+        console.log(res);
+    })
+});
