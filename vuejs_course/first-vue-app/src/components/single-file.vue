8c58805a65da2feb6ee2efa0c308ec0f240c3d13
Joan <113860964+JoanGarfias@users.noreply.github.com>
2025-05-19 23:27:15 -0600

    vuejs course sesion

---
+++ vuejs_course/first-vue-app/src/components/single-file.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/*Acá va toda la lógica de JavaScript o Typescript */
+    import { ref } from "vue";
+    const saludo = ref("Hola mundo desde un componente de un solo archivo");
+</script>
+
+<template>
+    <!-- HTML - ESTRUCTURA DEL COMPONENTE -->
+    <div class="container">
+        {{ saludo }}
+    </div>
+</template>
+
+<style scoped>
+    /**El scoped nos permite que otros estilos no sobrescriban los estilos de este componente apesar
+    de que se llamen igual en clases o id.
+    */
+    .container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        align-items: center;
+        text-align: center;
+    }
+</style>
