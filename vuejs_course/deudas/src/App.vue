5b33e337f06c5eef7c1eae01545206c470f285df
Joan <113860964+JoanGarfias@users.noreply.github.com>
2025-05-26 08:56:55 -0600

    deudas version final

--- vuejs_course/deudas/src/App.vue
+++ vuejs_course/deudas/src/App.vue
@@ -1,10 +1,15 @@
 <script>
 import { reactive } from 'vue';
+import { ref } from 'vue';
 
 export default{
     
     data(){
+
         return {
+            porPagar: ref(0),
+            pagado: ref(0),
+            total: ref(0),
             newEntry: {
                 name: "",
                 amount: 0
@@ -23,9 +28,50 @@ export default{
             ])
         }
     },
+    methods: {
+        add() {
+            if(this.newEntry.name && this.newEntry.amount > 0){
+                this.lista.push({
+                    id: this.lista.length + 1,
+                    name: this.newEntry.name,
+                    amount: parseFloat(this.newEntry.amount),
+                    paid: false
+                });
+                this.newEntry.name = "";
+                this.newEntry.amount = 0;
+            } else {
+                alert("Por favor, completa todos los campos.");
+            }
+        },
+        remove(index){
+            if(confirm("¿Estás seguro de eliminar esta deuda?")){
+                this.lista.splice(index, 1);
+            }
+        },
+        changeStatePaid(item){
+            item.paid = !item.paid;
+        },
+    },
     components(){
 
-    }
+    },
+    computed: {
+        pagosCalculados() {
+            let pagado = 0;
+            let porPagar = 0;
+
+            for (const item of this.lista) {
+            if (item.paid) pagado += item.amount;
+            else porPagar += item.amount;
+            }
+
+            return {
+            pagado,
+            porPagar,
+            total: pagado + porPagar
+            };
+        }
+    },
 }
 
 </script>
@@ -51,26 +97,26 @@ export default{
                             <tr>
                                 <td></td>
                                 <td>
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" v-model="newEntry.name" placeholder="Descripción de la deuda">
                                 </td>
                                 <td>
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" v-model="newEntry.amount" placeholder="Monto de la deuda">
                                 </td>
                                 <td>
-                                    <button type="button" class="btn btn-success btn-block">Agregar</button>
+                                    <button type="button" class="btn btn-success btn-block" @click="add()">Agregar</button>
                                 </td>
                             </tr>
 
-                            <tr v-for="item in lista" :key="item.id">
+                            <tr v-for="(item, index) in lista" :key="index">
                                 <td>
-                                    <button type="button" class="btn btn-danger btn-xs">
+                                    <button type="button" class="btn btn-danger btn-xs" @click="remove(index)">
                                         <i class="glyphicon glyphicon-trash"></i>
                                     </button>
                                 </td>
                                 <td>{{ item.name }}</td>
                                 <td>{{ item.amount }}</td>
                                 <td class="text-center" :title="item.paid ? 'Si' : 'No'">
-                                    <button class="btn btn-default btn-sm" :class="{'btn-success': item.paid}">
+                                    <button class="btn btn-default btn-sm" :class="{'btn-success': item.paid}" @click="changeStatePaid(item)">
                                         <i v-if="item.paid" class="glyphicon glyphicon-ok"></i>
                                         <i v-if="!item.paid" class="glyphicon glyphicon-remove"></i>
                                     </button>
@@ -91,21 +137,21 @@ export default{
                             <tr class="text-danger">
                                 <td></td>
                                 <td class="text-right">Por pagar</td>
-                                <td>0</td>
+                                <td>{{ pagosCalculados.porPagar }}</td>
                                 <td></td>
                             </tr>
 
                             <tr class="text-success">
                                <td></td>
                                <td class="text-right">Pagado</td>
-                               <td>0</td>
+                               <td>{{ pagosCalculados.pagado }}</td>
                                <td></td> 
                             </tr>
 
                             <tr class="text-info">
                                 <td></td>
                                 <td class="text-right">Total</td>
-                                <td>0</td>
+                                <td>{{ pagosCalculados.total }}</td>
                                 <td></td>
                             </tr>
 
